22fd4a5e51f2ae458303a450491557a5611f7b38
dthill <dthill@gmx.net>
2022-09-03 16:37:57 +0200

    login with selenium

--- selenium/src/main/java/LoginTest.java
+++ selenium/src/main/java/LoginTest.java
@@ -1,2 +1,36 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.time.Duration;
+
 public class LoginTest {
+
+    public void run() {
+        System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
+        WebDriver driver;
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        WebElement textBox = driver.findElement(By.id("my-text-id"));
+        WebElement passwordBox = driver.findElement(By.name("my-password"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        new Actions(driver)
+                .moveToElement(textBox)
+                .sendKeys("Username")
+                .moveToElement(passwordBox)
+                .sendKeys("Password123")
+                .moveToElement(submitButton)
+                .click()
+                .pause(Duration.ofSeconds(1))
+                .perform();
+        WebElement message = driver.findElement(By.id("message"));
+        if (message.getText().equals("Received!")) {
+            System.out.println("Login success");
+        } else {
+            System.out.println("login failed");
+        }
+    }
 }
--- selenium/src/main/java/Main.java
+++ selenium/src/main/java/Main.java
@@ -1,35 +1,6 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
-import java.time.Duration;
-
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
-        WebDriver driver;
-        driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
-        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
-        WebElement textBox = driver.findElement(By.id("my-text-id"));
-        WebElement passwordBox = driver.findElement(By.name("my-password"));
-        WebElement submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
-        new Actions(driver)
-                .moveToElement(textBox)
-                .sendKeys("Username")
-                .moveToElement(passwordBox)
-                .sendKeys("Password123")
-                .moveToElement(submitButton)
-                .click()
-                .pause(Duration.ofSeconds(1))
-                .perform();
-        WebElement message = driver.findElement(By.id("message"));
-        if(message.getText().contains("Received|")){
-            System.out.println("Login success");
-        } else {
-            System.out.println("login failed");
-        }
+        new LoginTest().run();
     }
 
 }
