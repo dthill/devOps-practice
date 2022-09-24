61102b35e347d47c2ad580855329cfb2c09469bc
dthill <dthill@gmx.net>
2022-09-24 14:14:35 +0200

    add jenkins files

---
+++ jenkins/setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+echo Installing Docker
+
+sudo apt-get --yes remove docker docker.io containerd runc
+sudo apt-get --yes update
+sudo apt-get --yes install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt-get --yes update
+sudo apt-get --yes install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get --yes install openjdk-11-jre
+sudo apt-get --yes install jenkins
+sudo usermod -a -G docker jenkins
+
+sudo service docker start
+
+
